29dec64a800530c9f01ae9dc1d75ef5b72352c6c
tobi5775 <tobias.wittmann96@web.de>
2022-03-21 13:46:51 +0100

    add thumbprint for openid connect provider

--- karpenter.tf
+++ karpenter.tf
@@ -7,9 +7,13 @@ module "iam_assumable_role_karpenter" {
   oidc_fully_qualified_subjects = ["system:serviceaccount:karpenter:karpenter"]
 }
 
+data "tls_certificate" "cluster" {
+  url = module.eks.cluster_oidc_issuer_url
+}
+
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = []
+  thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
   url             = module.eks.cluster_oidc_issuer_url
 }
 
